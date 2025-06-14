d89db4bf74744cbd805521b108576ac2d296f71a
Worakrit Kullanatpokin <worrakit.boss@gmail.com>
2025-06-14 18:26:10 +0700

    fix: remove signup

--- src/app/signin/page.tsx
+++ src/app/signin/page.tsx
@@ -43,7 +43,7 @@ export default function SignIn() {
   };
 
   return (
-    <div className="min-h-screen flex items-center justify-center bg-gradient-to-b from-blue-50 to-white py-12 px-4 sm:px-6 lg:px-8">
+    <div className="min-h-screen flex items-center justify-center bg-gradient-to-b  bg-white py-12 px-4 sm:px-6 lg:px-8">
       <div className="max-w-md w-full space-y-8">
         <div>
           <h2 className="mt-6 text-center text-3xl font-extrabold text-gray-900">
@@ -180,18 +180,6 @@ export default function SignIn() {
             </button>
           </div>
         </form>
-
-        <div className="text-center">
-          <p className="text-sm text-gray-600">
-            ยังไม่มีบัญชี?{" "}
-            <Link
-              href="/signup"
-              className="font-medium text-blue-600 hover:text-blue-500"
-            >
-              สมัครสมาชิก
-            </Link>
-          </p>
-        </div>
       </div>
     </div>
   );
