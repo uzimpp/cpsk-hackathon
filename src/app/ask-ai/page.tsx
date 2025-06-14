c985c38f7b4b999dd5315827ae3cb37cc7d1f4bd
Worakrit Kullanatpokin <worrakit.boss@gmail.com>
2025-06-14 12:58:58 +0700

    feat: adding more files for the project

---
+++ src/app/ask-ai/page.tsx
@@ -0,0 +1,3 @@
+export default function AskAI() {
+  return <div>AskAI</div>;
+}
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import NavBar from "@/components/global/NavBar";
+import Footer from "@/components/global/Footer";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -27,7 +29,9 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        {children}
+        <NavBar />
+        <main className="h-screen p-6">{children}</main>
+        <Footer />
       </body>
     </html>
   );
